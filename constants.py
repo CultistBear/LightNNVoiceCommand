3fa10f15e2a66c5a6a3aeee788f0d0653bdecc32
Kamya Singh <singh.kamya.ks98@gmail.com>
2025-05-04 17:09:31 +0530

    Update constants.py

--- constants.py
+++ constants.py
@@ -4,3 +4,4 @@ MY_API_KEY_PORC = r""
 RISABH_API_KEY_PORC = r""
 CURRENT_WORKING_DIR = pathlib.Path(__file__).parent.resolve()
 PARENT_DIR = pathlib.Path(CURRENT_WORKING_DIR).parent.resolve()
+LATEST_TRAINED = r""
